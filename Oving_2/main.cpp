a343e219e4193449e2b822a461410c3385d6159b
nfsemb <nfsemb@stud.ntnu.no>
2019-09-18 08:49:52 +0200

    Initial Commit

---
+++ Oving_2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Oving_2
+//
+//  Created by Niels Sole Semb on 18/09/2019.
+//  Copyright © 2019 Niels Sole Semb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
